a00425c073062a1bc953758ea85595e55963b062
rakesh050791 <rakesh050791@gmail.com>
2023-11-30 01:35:47 +0100

    1083. Sales Analysis II197. Rising Temperature

---
+++ Easy/1083-Sales-Analysis-II.sql
@@ -0,0 +1,98 @@
+-- 1083. Sales Analysis II
+-- https://leetcode.com/problems/sales-analysis-ii/description/
+
+-- Table: Product
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | product_id   | int     |
+-- | product_name | varchar |
+-- | unit_price   | int     |
+-- +--------------+---------+
+-- product_id is the primary key (column with unique values) of this table.
+-- Each row of this table indicates the name and the price of each product.
+-- Table: Sales
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | seller_id   | int     |
+-- | product_id  | int     |
+-- | buyer_id    | int     |
+-- | sale_date   | date    |
+-- | quantity    | int     |
+-- | price       | int     |
+-- +-------------+---------+
+-- This table might have repeated rows.
+-- product_id is a foreign key (reference column) to the Product table.
+-- buyer_id is never NULL. 
+-- sale_date is never NULL. 
+-- Each row of this table contains some information about one sale.
+ 
+
+-- Write a solution to report the buyers who have bought S8 but not iPhone. Note that S8 and iPhone are products presented in the Product table.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Product table:
+-- +------------+--------------+------------+
+-- | product_id | product_name | unit_price |
+-- +------------+--------------+------------+
+-- | 1          | S8           | 1000       |
+-- | 2          | G4           | 800        |
+-- | 3          | iPhone       | 1400       |
+-- +------------+--------------+------------+
+-- Sales table:
+-- +-----------+------------+----------+------------+----------+-------+
+-- | seller_id | product_id | buyer_id | sale_date  | quantity | price |
+-- +-----------+------------+----------+------------+----------+-------+
+-- | 1         | 1          | 1        | 2019-01-21 | 2        | 2000  |
+-- | 1         | 2          | 2        | 2019-02-17 | 1        | 800   |
+-- | 2         | 1          | 3        | 2019-06-02 | 1        | 800   |
+-- | 3         | 3          | 3        | 2019-05-13 | 2        | 2800  |
+-- +-----------+------------+----------+------------+----------+-------+
+-- Output: 
+-- +-------------+
+-- | buyer_id    |
+-- +-------------+
+-- | 1           |
+-- +-------------+
+-- Explanation: The buyer with id 1 bought an S8 but did not buy an iPhone. The buyer with id 3 bought both.
+
+-- HINT 
+
+-- EXCEPT operator returns rows in A that are not in B. The output has distinct rows, and entity integrity is ensured.
+
+-- Approach 1 
+
+SELECT buyer_id
+FROM Sales s
+INNER JOIN Product p
+ON s.product_id = p.product_id
+WHERE p.product_name = 'S8'
+
+EXCEPT
+
+SELECT buyer_id
+FROM Sales s
+INNER JOIN Product p
+ON s.product_id = p.product_id
+WHERE p.product_name = 'iPhone'
+
+
+-- Approach 2 
+
+select buyer_id 
+from Sales s 
+join Product p 
+on s.product_id = p.product_id
+group by buyer_id
+having sum(product_name='S8') > 0 and sum(product_name='iPhone') = 0
